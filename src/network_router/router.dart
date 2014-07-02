1870ba42577a55dfe191589927e5798b5732fb75
Morten Nygaard Åsnes <morten.aasnes@gmail.com>
2014-07-02 07:45:52 +0200

    Finished up the Network Router challenge

--- src/network_router/router.dart
+++ src/network_router/router.dart
@@ -9,8 +9,7 @@ class Edge {
     n1.edges.add(this);
     n2.edges.add(this);
   }
-  bool operator==(other) => (n1 == other.n1 && n2 == other.n2) || 
-                            (n1 == other.n2 && n2 == other.n1);
+  bool operator ==(other) => (n1 == other.n1 && n2 == other.n2) || (n1 == other.n2 && n2 == other.n1);
   int get hashCode => n1.hashCode + n2.hashCode;
 }
 
@@ -18,10 +17,11 @@ class Node {
   Set<Edge> edges = new Set();
   String label;
   bool visited = false;
-  num distance = double.MAX_FINITE; 
-  
+  num distance = double.MAX_FINITE;
+  Node previous = null;
+
   Node(this.label);
-  bool operator==(other) => label == other.label;
+  bool operator ==(other) => label == other.label;
   int get hashCode => label.hashCode;
   List<Node> reachable() {
     var nodes = new Set<Node>();
@@ -34,7 +34,7 @@ class Node {
 }
 
 class Graph {
-  
+
   Set<Edge> edges;
   Set<Node> nodes;
 
@@ -48,7 +48,7 @@ class Graph {
     for (int i = 0; i < h; i++) {
       for (int j = i + 1; j < w; j++) {
         num w = m[i][j];
-        if(w >= 0) {
+        if (w >= 0) {
           Node n1 = getOrAddNode(labels[i]);
           Node n2 = getOrAddNode(labels[j]);
           Edge e = new Edge.connect(n1, n2, w);
@@ -57,23 +57,52 @@ class Graph {
       }
     }
   }
-  
+
   List<Node> shortestRoute(Node s, Node e) {
     nodes.forEach((n) {
-      n.visited = false; 
+      n.visited = false;
       n.distance = double.MAX_FINITE;
+      n.previous = null;
     });
     Node start = nodes.lookup(s);
     Node end = nodes.lookup(e);
     start.distance = 0;
-    var queue = new SplayTreeMap<Node, Node>.fromIterable(nodes.toList(),
-                    compare: (n1, n2) => n1.distance.compareTo(n2.distance));
-    queue.forEach((n1, _) {
-      print('${n1.label} ${n1.distance}');
+    var queue = new SplayTreeMap<Node, Node>((n1, n2) {
+      var cmp = n1.distance.compareTo(n2.distance);
+      return cmp == 0 ? n1.label.compareTo(n2.label) : cmp;
     });
-    return null;
+    nodes.forEach((n) {
+      queue[n] = n;
+    });
+
+    var current = start;
+    while (!end.visited) {
+      for (var edge in current.edges) {
+        var node = (current == edge.n1 ? edge.n2 : edge.n1);
+        if (!node.visited) {
+          var d = current.distance + edge.weight;
+          if (d < node.distance) {
+            queue.remove(node);
+            node.distance = d;
+            node.previous = current;
+            queue[node] = node;
+          }
+        }
+      }
+      queue.remove(current);
+      current.visited = true;
+      current = queue.firstKey();
+    }
+    
+    var path = [];
+    var n = end;
+    while(n != null) {
+      path.add(n);
+      n = n.previous;
+    }
+    return path.reversed.toList();
   }
-  
+
   Node getOrAddNode(String label) {
     Node n = new Node(label);
     if (nodes.contains(n)) {
@@ -83,7 +112,6 @@ class Graph {
     }
     return n;
   }
-  
 }
 
 void main(List<String> args) {
@@ -99,12 +127,13 @@ void main(List<String> args) {
   var end = startAndEnd.last.trim();
   assert(lines.length == size + 2);
 
-  print(lines.length);
   var m = new List<List<num>>();
-  for (int i = 1; i<=size; i++) {
+  for (int i = 1; i <= size; i++) {
     m.add(lines[i].trim().split(',').map(num.parse).toList());
     assert(m.last.length == size);
   }
   var g = new Graph.fromDistanceMatrix(m);
-  g.shortestRoute(new Node(start), new Node(end));
+  var path = g.shortestRoute(new Node(start), new Node(end));
+  print(path.last.distance);
+  print(path.map((n) => n.label).join(''));
 }
